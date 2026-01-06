923fd74410cbe8df9a36fb5222a0fc397033df81
surajcodes017 <suraj.codes017@gmail.com>
2026-01-06 11:47:04 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = false
+
+// console.log(typeof(score))
+// console.log(typeof score) 
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(typeof NaN)
+
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// so what i learn is when we are converting into the boolean values , when we give empty string 
+// it will convert to false , and when we give any string it will convert to true
+// 1 => true , 0 => false
+// " ", "suraj"  =>  true,   "" => false
+
+
+// string converison
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
