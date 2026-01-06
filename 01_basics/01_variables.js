1623c4674cc69c0c49fe59e618f4975378471493
surajcodes017 <suraj.codes017@gmail.com>
2026-01-06 11:21:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "surajduppally27@gmail.com"
+var accountPassword = "@abcd"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2233
+accountEmail = "newsuraj@dube.com"
+accountPassword = "change@pass"
+accountCity = "Chennai"
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as new version
+
+
+// alert( 3+2)   // we are using node js, not browser
+
+let name ="suraj"
+let age = 21
+let isLoggedIn = true 
+let state = null
+
+// These are all Primitive Data types
+
+//number => 2 to power 53
+// bitint
+//string => ""
+//boolean => true/false
+// null =>  standalone value
+//undefined => till now value did  ot get assigned
+//symbol =>  used mainly in react , used mainly for uniqueness
+
+//Non-Primituve datatypes
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
