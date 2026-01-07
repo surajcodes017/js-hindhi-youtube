0494382d6739452639dea907a48697b63ed36ebb
surajcodes017 <suraj.codes017@gmail.com>
2026-01-07 11:11:51 +0000

    data types summary

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -12,20 +12,41 @@
 // console.log(typeof NaN)
 
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // so what i learn is when we are converting into the boolean values , when we give empty string 
 // it will convert to false , and when we give any string it will convert to true
 // 1 => true , 0 => false
-// " ", "suraj"  =>  true,   "" => false
+// " ", "suraj"  =>  true,   "",null,undefined,NaN => false
 
 
 // string converison
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+//   ********* operations *********
+
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+
+
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
