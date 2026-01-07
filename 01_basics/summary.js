a47a78c2fbec6a05a6d7d82ff32ae58d6209cb1a
surajcodes017 <suraj.codes017@gmail.com>
2026-01-07 11:13:41 +0000

    data types summary

---
+++ 01_basics/summary.js
@@ -0,0 +1,11 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+
+const outsideTemp = null
+console.log(outsideTemp)
+let userEmail;
+console.log(userEmail)
+
+const cricketers = ["virat","rohit","hardik","dhoni"]
+console.log(typeof cricketers)
