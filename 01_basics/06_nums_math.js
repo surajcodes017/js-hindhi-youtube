d218f6718a2383b2df078e83c87dbc12106b95e0
surajcodes017 <suraj.codes017@gmail.com>
2026-01-08 10:13:23 +0000

    Maths and Numbers

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score)
+
+
+
+// const balance = new Number(100)
+// console.log(balance)
+
+
+// console.log(typeof balance)
+// console.log(balance.toString())
+
+// console.log(typeof balance.toString())
+// console.log(typeof balance)
+// console.log(balance.toString().length)
+
+
+const  num = new Number(84)
+// console.log(num.toFixed())
+
+const otherNum = 59.8966
+// console.log(otherNum.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++++ Maths +++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.33))
+// console.log(Math.ceil(4.33))
+// console.log(Math.floor(4.33))
+// console.log(Math.min(1,2,3,4,5,6))
+// console.log(Math.max(1,2,3,4,5,6))
+
+// console.log(Math.random() )
+// console.log(Math.floor(Math.random()*10 + 1))
+
+const min= 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
