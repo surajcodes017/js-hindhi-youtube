10725a5a1fbdae06a1456ed0dc80037b9541c828
surajcodes017 <suraj.codes017@gmail.com>
2026-01-13 17:03:52 +0000

    Loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,8 +1,30 @@
 //for loop 
 
 for(let i=0;i<10;i++){
-    var ele = i;
-    console.log(ele)
+    const ele = i;
+    // console.log(ele)
 }
 
-consoel.log(el)
+// console.log(ele) 
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`outer LOOP:  ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner LOOP ${j,i}`)
+        
+    }
+    
+}
+
+// While loop 
+let i=0;
+
+while (i<=10) {
+    // console.log(`value of index ${i}`)
+    i+=2
+}
+let score =9
+do {
+    console.log(`score is ${score}`)
+    score++; 
+} while (score<10);
